f3f31f47053dc3e3b12c7ed30d3e54cf7f887158
p0lluxstar <web-registra@yandex.ru>
2025-03-04 00:26:15 +0300

    feat: add table ClimateData;

--- display/models.py
+++ display/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ClimateData(models.Model):
+    temperature = models.FloatField(verbose_name="Temperature")
+    humidity = models.FloatField(verbose_name="Humidity")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Date")
+
+    def __str__(self):
+        return f"{self.timestamp}: {self.temperature}°C, {self.humidity}%"
+
